95a7c63a93c4d1fcbcfadd33403bebf3d52993fb
emanuel lopez <emanuel-lopez13@hotmail.com>
2022-07-21 21:38:21 -0300

    Agregando Nav al proyecto

---
+++ src/Nav.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function Nav() {
+  return (
+
+    <nav className="App-Nav">
+      <div className="Logo-Nav">
+        <h1>TiendaShoop</h1>
+      </div>
+      <div className="Link-Nav">
+        <a href="sasa">Home</a>
+        <a href="sasa"> Productos</a>
+        <a href="sasa">Quienes somos</a>
+        <a href="sasa">Contacto</a>
+      </div>
+    </nav>
+
+
+  )
+}
+
+export default Nav;
